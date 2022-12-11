f955dc9a796bf48e361419a2fb672d24b10e4a4f
denis46-g <63370054+denis46-g@users.noreply.github.com>
2022-12-11 20:55:34 +0300

    add tests

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+import unittest
+
+
 def factorial(num):
     if num == 1 or num == 0:
         return 1
     else:
-        return num * factor(num - 1)
+        return num * factorial(num - 1)
+
+
+class FactorialTestCase(unittest.TestCase):
+    def test1(self):
+        res = factorial(5)
+        self.assertEqual(res, 120)
+
+    def test2(self):
+        res = factorial(6)
+        self.assertEqual(res, 720)
+
+    def test3(self):
+        res = factorial(0)
+        self.assertEqual(res, 1)
 
 
 if __name__ == '__main__':
